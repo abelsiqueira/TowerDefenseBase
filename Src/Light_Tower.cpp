a72c5918ed2f7df09ba57ef7dc235c8e033f709c
Abel Siqueira <abel.s.siqueira@gmail.com>
2012-06-18 01:21:08 -0300

    Add an example

--- Src/Light_Tower.cpp
+++ Src/Light_Tower.cpp
@@ -5,7 +5,8 @@ void LightTower::Update () {
 }
 
 void LightTower::Draw () {
-  float x0 = mPosition.x - 5, y0 = mPosition.y - 5;
-  float x1 = mPosition.x + 5, y1 = mPosition.y + 5;
+  int halfSide = 15;
+  float x0 = mPosition.x - halfSide, y0 = mPosition.y - halfSide;
+  float x1 = mPosition.x + halfSide, y1 = mPosition.y + halfSide;
   mDrawingClass->DrawFilledRectangle(x0, y0, x1, y1, 0, 255, 255);
 }
--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 int main () {
   Interface interface;
 
+  interface.CreateTower (TT_LightTower, 50, 50);
+  interface.CreateTower (TT_LightTower, 150, 50);
+  interface.CreateEnemy (ET_Skeleton,   50, 150);
+  interface.CreateEnemy (ET_Skeleton,  150, 150);
   interface.Run();
 
   return 0;
